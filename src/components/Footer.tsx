15e332714f0021662fca1c5ffb36faf75b2be858
IamRKG <radhacreative@gmail.com>
2024-08-14 17:58:27 +0530

    Name Change (#13)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
   return (
     <footer className="bg-orange-900 text-white p-4 mt-8">
       <div className="container mx-auto text-center">
-        <p>&copy; 2024 PATHIYAM. All rights reserved.</p>
+        <p>&copy; 2024 Pathayam. All rights reserved.</p>
         <div className="mt-2">
           <a href="/privacy" className="hover:text-blue-300 mr-4">Privacy Policy</a>
           <a href="/terms" className="hover:text-blue-300">Terms of Service</a>
